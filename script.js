05fa14ffe9562bde26c184186f6472d2892d6e2c
ZxYerSixZx <oroscoyerson2019@gmail.com>
2025-02-07 22:29:57 -0500

    Actualizar

---
+++ script.js
@@ -0,0 +1,5 @@
+function openLetter() {
+    document.querySelector('.letter').style.transform = 'translateY(0)';
+    document.querySelector('.letter').style.opacity = '1';
+    document.getElementById('music').play();
+}
