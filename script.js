a3b30878b16a21c277a87fc482e3dd96391cb9b7
ZxYerSixZx <oroscoyerson2019@gmail.com>
2025-02-07 22:32:27 -0500

    Actualizar script.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 function openLetter() {
     document.querySelector('.letter').style.transform = 'translateY(0)';
     document.querySelector('.letter').style.opacity = '1';
-    document.getElementById('music').play();
 }
